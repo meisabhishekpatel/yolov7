e3409b93697efe57cd56008e1fe995e25b79dfec
Abhishek Patel <71618624+meisabhishekpatel@users.noreply.github.com>
2024-02-23 02:12:36 +0530

    Update wandb_utils.py

--- utils/wandb_logging/wandb_utils.py
+++ utils/wandb_logging/wandb_utils.py
@@ -12,9 +12,9 @@ from utils.datasets import img2label_paths
 from utils.general import colorstr, xywh2xyxy, check_dataset
 
 # try:
-    # import wandb
-    # from wandb import init, finish
-# except ImportError:
+    import wandb
+    from wandb import init, finish
+except ImportError:
 wandb = None
 
 WANDB_ARTIFACT_PREFIX = 'wandb-artifact://'
